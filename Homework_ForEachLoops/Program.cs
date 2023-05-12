39bfddccc65c20009fe92b39676035b1b73e30cb
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-12 00:50:28 -0600

    code complete.

--- Homework_ForEachLoops/Program.cs
+++ Homework_ForEachLoops/Program.cs
@@ -1,1 +1,23 @@
 ﻿
+// Ask user for their first name.
+// Continue asking for first names until exit
+// Loop through each name using foreach saying hello
+
+//ask user for first name
+string? response;
+List<string> names = new();
+do
+{
+    Console.Write("What is your first name (e to exit): ");
+    response = Console.ReadLine();
+    if (response != "e")
+    {
+        names.Add(response);
+    }
+} while (response != "e");
+
+//loop through each name in list
+foreach (string name in names)
+{
+    Console.WriteLine($"Hello {name}.");
+}
